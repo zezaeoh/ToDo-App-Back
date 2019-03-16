f1cb07500bae37f765a3e3a2b03c7376a100fa2c
jaegeon <zezaeoh@gmail.com>
2019-03-16 09:30:05 +0000

    add cors allow url
    tmp

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(bodyParser.json());
 // CORS
 app.use(express.json());
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://101.101.162.150:3000");
+    res.header("Access-Control-Allow-Origin", "http://101.101.162.150");
     res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
